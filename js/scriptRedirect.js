dae362f6948a1e33e78f05710572976c3fe75a07
efranandiAR <83424201+efranandiAR@users.noreply.github.com>
2021-10-05 16:33:50 +0700

    Add files via upload

---
+++ js/scriptRedirect.js
@@ -0,0 +1,11 @@
+function back(){
+    location.replace("./index.html")
+}
+
+// if(typeof(Storage) !== "undefined"){
+//     let getName = sessionStorage.getItem("name");
+//     console.log(getName);
+//     document.getElementById("uName").innerHTML = sessionStorage.getItem("name");
+// } else{
+//     alert("Error: Browser does not support Web Storage")
+// }
